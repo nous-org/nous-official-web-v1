ac2e11e6817621aa006630b13729272df349b8d9
nous-adg <alessandro@nous.cr>
2025-09-26 10:56:20 -0600

    fixing details in structure

--- src/components/react/timeLine/TimeLineCta.tsx
+++ src/components/react/timeLine/TimeLineCta.tsx
@@ -12,7 +12,7 @@ export const TimeLineCta = () => {
                 <h2 className="text-2xl font-bold sm:text-4xl md:text-5xl lg:text-6xl text-black dark:text-white max-w-4xl text-center mb-4">Start your project now</h2>
                 <h3 className="text-md font-bold sm:text-lg md:text-3xl lg:text-4xl text-black dark:text-white max-w-4xl text-center">We’re one conversation away.</h3>
                 <p className="text-neutral-700 dark:text-neutral-300 text-md md:text-lg lg:text-xl text-center py-8">We turn ambition into momentum—clear plan, short sprints, and outcomes you can feel in weeks.</p>
-                <footer className='flex gap-4'>
+                <footer className='flex flex-col md:flex-row gap-4'>
                     <ButtonVariant
                         text="Book a 15-min intro call "
                         icon={false}
